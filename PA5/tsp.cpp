8b815d80c9355711257f567871f28772cd02606a
gges5110 <gges5110@gmail.com>
2016-07-19 23:38:24 -0500

    PA5 minor improvement

--- PA5/tsp.cpp
+++ PA5/tsp.cpp
@@ -61,7 +61,11 @@ public:
 
         // TODO: need to come up with a better hash function.
         // return (31 * key.vertex + fn(key.set));
-        return (31 * key.vertex);
+        int sum = 0;
+        for (int i: key.set) {
+            sum += i;
+        }
+        return (31 * key.vertex + sum);
     }
 };
 
@@ -101,30 +105,6 @@ private:
         }
     }
 
-    void load_test_distance() {
-        distance_matrix = new double*[node_size];
-        for (int i = 0; i < node_size; ++i) {
-            distance_matrix[i] = new double[node_size];
-        }
-
-        distance_matrix[0][0] = 0;
-        distance_matrix[0][1] = 1;
-        distance_matrix[0][2] = 15;
-        distance_matrix[0][3] = 6;
-        distance_matrix[1][0] = 2;
-        distance_matrix[1][1] = 0;
-        distance_matrix[1][2] = 7;
-        distance_matrix[1][3] = 3;
-        distance_matrix[2][0] = 9;
-        distance_matrix[2][1] = 6;
-        distance_matrix[2][2] = 0;
-        distance_matrix[2][3] = 12;
-        distance_matrix[3][0] = 10;
-        distance_matrix[3][1] = 4;
-        distance_matrix[3][2] = 8;
-        distance_matrix[3][3] = 0;
-    }
-
     vector<int> complimentary(vector<int> set, int size) {
         vector<int> answer;
         for (int i = 1; i < size; ++i) {
@@ -142,22 +122,6 @@ private:
             temp_set.insert(i);
         }
         return temp_set;
-    }
-
-public:
-    TSP(ifstream& fs) {
-        fs >> node_size;
-        nodes = new Coordinate<double>[node_size];
-        double x, y;
-
-        for (int line = 0; line < node_size; ++line) {
-            fs >> x >> y;
-            Coordinate<double> temp(x, y);
-            nodes[line] = temp;
-        }
-
-        calculate_distances();
-        minCost();
     }
 
     vector<vector<int>> combinations(int k) {
@@ -180,12 +144,23 @@ public:
         }
     }
 
-    TSP() {
-        node_size = 4;
+public:
+    TSP(ifstream& fs) {
+        fs >> node_size;
         nodes = new Coordinate<double>[node_size];
-        load_test_distance();
+        double x, y;
+
+        for (int line = 0; line < node_size; ++line) {
+            fs >> x >> y;
+            Coordinate<double> temp(x, y);
+            nodes[line] = temp;
+        }
+
+        calculate_distances();
         minCost();
     }
+
+
 
     void minCost() {
         unordered_map<Set_vertex, Cost_parent, KeyHasher> map;
@@ -197,57 +172,50 @@ public:
             map.insert({setVertex, costParent});
         }
 
-
+        unordered_map<Set_vertex, Cost_parent, KeyHasher> new_map;
         for (int set_size = 1; set_size < node_size - 1; ++set_size) {
             vector<vector<int>> S = combinations(set_size);
             for (vector<int> set: S) {
-                vector<int> complimentary_set = complimentary(set, node_size);
-                for (int k: complimentary_set) {
+                for (int k: complimentary(set, node_size)) {
                     Cost_parent min_costParent = getMinCost(k, Vector_to_set(set), map);
                     Set_vertex now(k, Vector_to_set(set));
-                    map.insert({now, min_costParent});
+                    new_map.insert({now, min_costParent});
                 }
             }
+            map = new_map;
+            new_map.clear();
         }
 
         // [0, {1, 2, 3}]
-        vector<int> set;
+        unordered_set<int> set;
         for (int i = 1; i < node_size; ++i) {
-            set.push_back(i);
+            set.insert(i);
         }
-        Cost_parent min_costParent = getMinCost(0, Vector_to_set(set), map);
+        Cost_parent min_costParent = getMinCost(0, set, map);
         cout << "Final answer = " << min_costParent.cost << endl;
     }
 
-    Cost_parent getMinCost(int k, unordered_set<int> set, unordered_map<Set_vertex, Cost_parent, KeyHasher> &map) {
+    Cost_parent getMinCost(const int k, const unordered_set<int> &set, unordered_map<Set_vertex, Cost_parent, KeyHasher> &map) {
         double min = 100000;
-        Set_vertex min_setVertex;
+        // Set_vertex min_setVertex;
         Cost_parent min_costParent(min, -1);
 
-        cout << "Calculating node " << k << endl;
-
+        // cout << "Calculating node " << k << endl;
+        unordered_set<int> temp_set = set;
         for (int m: set) {
-            unordered_set<int> temp_set = set;
             temp_set.erase(m);
             Set_vertex temp_setVertex(m, temp_set);
-
-            cout << "vertex = " << temp_setVertex.vertex << ", set = ";
-            for (int i: temp_setVertex.set) {
-                cout << i << ", ";
-            }
-            cout << endl;
-
             Cost_parent temp_costParent = map[temp_setVertex];
-            // cout << "Cost = " << temp_costParent.cost + distance_matrix[m][k] << endl;
-
-            if (temp_costParent.cost + distance_matrix[m][k] < min) {
-                min = temp_costParent.cost + distance_matrix[m][k];
-                min_setVertex = temp_setVertex;
+            double cost = temp_costParent.cost + distance_matrix[m][k];
+            if (cost < min) {
+                min = cost;
+                // min_setVertex = temp_setVertex;
                 min_costParent = temp_costParent;
             }
+            temp_set.insert(m);
         }
         min_costParent.cost = min;
-        cout << "Min cost = " << min << endl;
+        // cout << "Min cost = " << min << endl;
         return min_costParent;
     }
 
