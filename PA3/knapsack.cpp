cfeed316ebf4e637640caac1311275c2c6445ac3
Yu-Chia Wu <gerrywu@utexas.edu>
2017-06-25 22:20:56 -0500

    PA3 refined

--- PA3/knapsack.cpp
+++ PA3/knapsack.cpp
@@ -1,63 +1,64 @@
 #include <iostream>
 #include <fstream>
-#include <string>
-#include <algorithm>
+#include <vector>
+#include <algorithm> // std::max
 
-using std::string;
-using std::ifstream;
+using std::vector;
 
 class Knapsack {
+private:
+  vector<int> _v, _w;
+  int _knapsack_size, _number_of_items;
 public:
-    int *v, *w;
-    int knapsack_size, number_of_items;
-    Knapsack(ifstream &fs) {
-        fs >> knapsack_size >> number_of_items;
+  Knapsack(int knapsack_size, int number_of_items, const vector<int> &v, const vector<int> &w) :
+  _knapsack_size(knapsack_size),
+  _number_of_items(number_of_items),
+  _v(v), _w(w) {}
 
-        // Memory allocation.
-        v = new int[number_of_items];
-        w = new int[number_of_items];
-
-        int *prev, *curr;
-        prev = new int[knapsack_size];
-        curr = new int[knapsack_size];
-        for (int j = 0; j < knapsack_size; ++j) {
-            prev[j] = 0;
+  int compute() const {
+    vector<int> prev(_knapsack_size, 0), curr(_knapsack_size, 0);
+    for (int i = 1; i < _number_of_items; ++i) {
+      for (int j = 0; j < _knapsack_size; ++j) {
+        if (j - _w[i] >= 0) {
+          curr[j] = std::max(prev[j], prev[j - _w[i]] + _v[i]);
+        } else {
+          curr[j] = prev[j];
         }
+      }
 
-        // Read file.
-        for (int i = 0; i < number_of_items; ++i) {
-            fs >> v[i] >> w[i];
-        }
-
-        for (int i = 1; i < number_of_items; ++i) {
-            for (int j = 0; j < knapsack_size; ++j) {
-                if (j - w[i] >= 0) {
-                    curr[j] = std::max(prev[j], prev[j - w[i]] + v[i]);
-                }
-                else {
-                    curr[j] = prev[j];
-                }
-            }
-
-            // Copy curr to prev.
-            for (int j = 0; j < knapsack_size; ++j) {
-                prev[j] = curr[j];
-            }
-        }
-
-        std::cout << "Answer = " << curr[knapsack_size - 1] << std::endl;
-
-        delete [] prev, curr;
+      // Copy curr to prev.
+      for (int j = 0; j < _knapsack_size; ++j) {
+        prev[j] = curr[j];
+      }
     }
 
-    ~Knapsack() {
-        delete [] v, w;
-    }
+    return curr[_knapsack_size - 1];
+  }
 };
 
-int main() {
-    const string filename("knapsack_big.txt");
-    ifstream fs(filename);
-    Knapsack knapsack(fs);
-    return 0;
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cout << "Please provide a valid filename for testing." << std::endl;
+  } else {
+    std::ifstream fs;
+    fs.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+    try {
+      fs.open(argv[1]);
+
+      // Original main
+      int knapsack_size, number_of_items;
+      fs >> knapsack_size >> number_of_items;
+      vector<int> v(number_of_items), w(number_of_items);
+      for (int i = 0; i < number_of_items; ++i) {
+          fs >> v[i] >> w[i];
+      }
+      Knapsack knapsack(knapsack_size, number_of_items, v, w);
+      std::cout << knapsack.compute() << std::endl;
+    } catch (std::ifstream::failure& e) {
+      std::cerr << "Exception opening/reading file" << std::endl;
+      std::cout << e.what() << std::endl;
+    }
+    fs.close();
+  }
+  return 0;
 }
